46d1d84440880dce7096420f9b4f86012d447791
Maxvaucelle <max.vaucelle@sfr.fr>
2023-03-13 14:58:38 +0100

    j'emmerde le css

--- js/carrousel.js
+++ js/carrousel.js
@@ -4,7 +4,7 @@ document.body.onload = function () {
     content = document.getElementById("content");
     g = document.getElementById("g");
     d = document.getElementById("d");
-    content.style.width = (1480 * nbr) + "px";
+    content.style.width = (2000 * nbr) + "px";
     for (i = 1; i <= nbr; i++) {
         div = document.createElement("div");
         div.className = "photo";
@@ -16,7 +16,7 @@ document.body.onload = function () {
 d.onclick = function () {
     if (p > -nbr + 1)
         p--;
-    content.style.transform = "translate(" + p * 1480 + "px)";
+    content.style.transform = "translate(" + p * 33 + "%)";
     content.style.transition = "all 0.5s ease";
     afficherMasquer();
 
@@ -25,7 +25,7 @@ d.onclick = function () {
 g.onclick = function () {
     if (p < 0)
         p++;
-    content.style.transform = "translate(" + p * 1480 + "px)";
+    content.style.transform = "translate(" + p * 33 + "%)";
     content.style.transition = "all 0.5s ease";
     afficherMasquer();
 }
