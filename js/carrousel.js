beca6d179beea60133b4f4e2ffa0928c13fd42db
Maxvaucelle <max.vaucelle@sfr.fr>
2023-03-13 16:25:16 +0100

    j'emmerde aussi les carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -39,3 +39,70 @@ function afficherMasquer(){
     else
     g.style.visibility="visible";
 }
+
+
+// test 1
+// var actual = 0;
+// var total = 3;
+
+// function addClass(elem, name) {
+//     elem.className = elem.className + " " + name;
+// }
+
+// function deleteClass(elem, name) {
+//     var c = elem.className;
+//     elem.className = c.replace(name, "").replace(/   /g, " ").replace(/^ | $/g, "");
+// }
+
+// function nextImg() {
+//     var e;
+
+//     e = document.getElementById("img" + actual);
+//     deleteClass(e, "visible");
+
+//     actual++;
+//     if (actual > total - 1) actual = 0;
+
+//     e = document.getElementById("img" + actual);
+//     addClass(e, "visible");
+// }
+
+// var slider = setInterval(nextImg, 3000);
+
+// test2
+// const sliderImages = document.querySelector('.slider-images');
+// const sliderArrows = document.querySelectorAll('.slider-arrow');
+// let currentSlide = 0;
+
+// function goToSlide(slide) {
+//   sliderImages.style.transform = translateX(-${slide * 33.333} "%");
+//   currentSlide = slide;
+// }
+
+// function slidePrev() {
+//   if (currentSlide === 0) {
+//     goToSlide(2);
+//   } else {
+//     goToSlide(currentSlide - 1);
+//   }
+// }
+
+// function slideNext() {
+//   if (currentSlide === 2) {
+//     goToSlide(0);
+//   } else {
+//     goToSlide(currentSlide + 1);
+//   }
+// }
+
+// sliderArrows.forEach(function(arrow) {
+//   arrow.addEventListener('click', function() {
+//     if (arrow.classList.contains('slider-arrow-left')) {
+//       slidePrev();
+//     } else {
+//       slideNext();
+//     }
+//   });
+// });
+
+// goToSlide(currentSlide);
